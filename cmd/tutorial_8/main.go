4d6e996f4856cf1b5fbbc9d3b4f8fe9775fdc770
Adam Howard <mupwebdesign@gmail.com>
2026-04-23 14:46:04 +0100

    goroutines

--- cmd/tutorial_8/main.go
+++ cmd/tutorial_8/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
+var m = sync.Mutex{}
+var wg = sync.WaitGroup{}
+var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
+var results = []string{}
+
+func main() {
+	t0 := time.Now()
+	for i := 0; i < len(dbData); i++ {
+		wg.Add(1)
+		go dbCall(i)
+	}
+	wg.Wait()
+	fmt.Printf("\nTotal execution time: %v", time.Since((t0)))
+	fmt.Printf("\nThe results are %v", results)
+}
+
+func dbCall(i int) {
+	var delay float32 = rand.Float32() * 2000
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	fmt.Println("The result from the db is:", dbData[i])
+	m.Lock()
+	results = append(results, dbData[i])
+	m.Unlock()
+	wg.Done()
 }
